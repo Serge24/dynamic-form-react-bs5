85387238c10d6f7bb974ecb63b89539b768a7028
Serge24 <sergestalaki@gmail.com>
2025-01-06 16:59:56 +0000

    Commit with new directives

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/*const path = require('path');
 
 module.exports = {
     entry: './src/index.ts', // Entry point of your app
@@ -19,4 +19,39 @@ module.exports = {
         path: path.resolve(__dirname, 'dist') // Output directory
     },
     mode: 'development'
+};*/
+
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    output: {
+        filename: 'bundle.js',          // Output file
+        path: path.resolve(__dirname, 'dist') // Output directory
+    },
+    mode: 'development'
 };
+
